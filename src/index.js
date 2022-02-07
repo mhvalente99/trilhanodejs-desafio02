2d2371ca8b2fcba347a0647196a2f6b81ca69710
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-02-07 19:42:32 -0300

    feat: implementando middleware para realizar a busca do usuario por id

--- src/index.js
+++ src/index.js
@@ -68,7 +68,16 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não encontrado pelo id' })
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
